4bf395a972b20a143fe848ef7b2cae5b0d8b53b4
Paulo <davifd642@gmail.com>
2026-04-05 15:04:05 -0300

    new: test new role

--- iam.tf
+++ iam.tf
@@ -13,6 +13,33 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : aws_iam_openid_connect_provider.oidc-git.arn
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : ["sts.amazonaws.com"]
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:DavidDevd/devops.ci.iac:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
 
